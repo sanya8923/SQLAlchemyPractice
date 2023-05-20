f3ef0051c244eb28b5184c25a1a77ea075194ef6
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-05-20 23:13:29 +0400

    add all operation insert from sqlalchemy core

--- main.py
+++ main.py
@@ -13,11 +13,15 @@ table = Table('table', metadata,
 
 metadata.create_all(engine)
 
+# The insert() SQL Expression Construct
+
 stmt1 = insert(table).values(name='Sasha', age=23, phone=89504398717)
 print(f'output 1 (stmt1): \t\t\t\t\t\t\t{stmt1}')
 
 compiled = stmt1.compile().params
 print(f'output 2 Keys and Values (stmt1): \t\t\t{compiled}')
+
+# Executing the Statement
 
 with engine.connect() as conn:
     result = conn.execute(stmt1)
@@ -25,18 +29,23 @@ with engine.connect() as conn:
     print(f'output 3 Cursor position(stmt1): \t\t\t{result.inserted_primary_key}')
     conn.commit()
 
-stmt2 = insert(table)
+# INSERT usually generates the “values” clause automatically
+
+print(f'\noutput 4 usually generates the “values” clause automatically \n{insert(table)}\n')
 
 with engine.connect() as conn:
     result = conn.execute(
-             insert(table),
-             [
-                 {'name': 'Masha', 'age': 19, 'phone': 89234398717},
-                 {'name': 'Gosha', 'age': 32, 'phone': 89234391411}
-             ]
+        insert(table).values
+        (
+            [
+                {'name': 'Masha', 'age': 19, 'phone': 89234398717},
+                {'name': 'Gosha', 'age': 32, 'phone': 89234391411}
+            ]
+        )
     )
-    print(f'output 4 (stmt2): \t\t\t\t\t\t\t{result}')
     conn.commit()
+
+# Scalar Method That Allows To Automatically Find The ID Of A User By His Username
 
 user_accounts = Table('user_account_table', metadata,
                       Column('id', Integer, primary_key=True),
@@ -63,5 +72,20 @@ with engine.connect() as conn:
     )
     conn.commit()
 
-print(f'output 5: {insert(user_accounts).values().compile(engine)}')
+print(f'output 5 Insert with scalar request:  \t\t{insert(user_accounts).values().compile(engine)}')
 
+# INSERT ... RETURNING
+
+insert_stmt = insert(user_accounts).returning(user_accounts.c.id, user_accounts.c.email)
+print(f'output 6 INSERT ... RETURNING: \t\t\t\t{insert_stmt}')
+print(f'output 7 INSERT ... RETURNING params: \t\t{insert_stmt.compile().params}')
+
+select_stmt = select(user_accounts.c.id, user_accounts.c.name + "@aol.com")
+insert_stmt = insert(user_accounts).from_select(
+              ['id', 'email'], select_stmt)
+
+print(f'\noutput 8 INSERT ... FROM SELECT:\n{insert_stmt.returning(user_accounts.c.id, user_accounts.c.email)}\n')
+
+# INSERT…FROM SELECT
+
+print(f'\noutput 9 INSERT ... FROM SELECT:\n{insert_stmt}\n')
