2930bceea4991b71da37c078a23bf1c64a967488
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-05-20 22:10:32 +0400

    added a scalar method that allows you to automatically find the id of a user by his username and its usage

--- main.py
+++ main.py
@@ -1,76 +1,67 @@
-from sqlalchemy import create_engine, Integer, String, ForeignKey, insert
-from sqlalchemy.orm import Session, DeclarativeBase, mapped_column
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, insert
+from sqlalchemy import select, bindparam
 
-engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
-session = Session(engine)
+engine = create_engine('sqlite:///your_database.db')
+metadata = MetaData()
 
+table = Table('table', metadata,
+              Column('id', Integer, primary_key=True),
+              Column('name', String(255)),
+              Column('age', Integer()),
+              Column('phone', Integer())
+              )
 
-class Base(DeclarativeBase):
-    pass
+metadata.create_all(engine)
 
+stmt1 = insert(table).values(name='Sasha', age=23, phone=89504398717)
+print(f'output 1 (stmt1): \t\t\t\t\t\t\t{stmt1}')
 
-class Users(Base):
-    __tablename__ = 'users_table'
-    id = mapped_column(Integer(), primary_key=True)
-    name = mapped_column(String(255))
+compiled = stmt1.compile().params
+print(f'output 2 Keys and Values (stmt1): \t\t\t{compiled}')
 
+with engine.connect() as conn:
+    result = conn.execute(stmt1)
 
-class Data(Base):
-    __tablename__ = 'users_data_table'
-    id = mapped_column(Integer(), primary_key=True)
-    user_id = mapped_column(Integer(), ForeignKey('users_table.id'), nullable=False)
-    rule = mapped_column(String(255))
-    age = mapped_column(String(255))
-    work = mapped_column(String(255))
+    print(f'output 3 Cursor position(stmt1): \t\t\t{result.inserted_primary_key}')
+    conn.commit()
 
+stmt2 = insert(table)
 
-Base.metadata.create_all(engine)
+with engine.connect() as conn:
+    result = conn.execute(
+             insert(table),
+             [
+                 {'name': 'Masha', 'age': 19, 'phone': 89234398717},
+                 {'name': 'Gosha', 'age': 32, 'phone': 89234391411}
+             ]
+    )
+    print(f'output 4 (stmt2): \t\t\t\t\t\t\t{result}')
+    conn.commit()
 
+user_accounts = Table('user_account_table', metadata,
+                      Column('id', Integer, primary_key=True),
+                      Column('name', String(255)),
+                      Column('email', String(255))
+                      )
 
-class OriginData:
-    @staticmethod
-    def original_data():
-        users_data = [dict(name='Anton', rule='parent', age='23', work='proger'),
-                      dict(name='Sveta', rule='child', age='23', work='tester'),
-                      dict(name='Dima', rule='parent', age='23', work='buyer'),
-                      dict(name='Sasha', rule='child', age='23', work='seller'),
-                      dict(name='Kirril', rule='parent', age='23', work='blogger'),
-                      dict(name='Vadim', rule='child', age='23', work='taxer')
-                      ]
-        return users_data
+metadata.create_all(engine)
 
+scalar_subq = (
+    select(user_accounts.c.id)
+    .where(user_accounts.c.name == bindparam("username"))
+    .scalar_subquery()
+)
 
-class DataHandler:
-    data = []
+with engine.connect() as conn:
+    result = conn.execute(
+        insert(user_accounts).values(id=scalar_subq),
+        [
+            {"username": "spongebob", "email_address": "spongebob@sqlalchemy.org"},
+            {"username": "sandy", "email_address": "sandy@sqlalchemy.org"},
+            {"username": "sandy", "email_address": "sandy@squirrelpower.org"},
+        ],
+    )
+    conn.commit()
 
-    user_id = None
-    data_id = None
-
-    name = None
-    rule = None
-    age = None
-    work = None
-
-    def __init__(self, *args):
-        self.data = args
-
-
-class DataSeparatorFromInsert(DataHandler):
-
-    def data_separator(self):
-        try:
-            users_db = Users()
-            for line in self.data:
-                for item in line:
-                    stmt = insert(users_db).values(name=item.name)
-                    print(stmt)
-        except Exception as e:
-            raise e
-            session.rollback()
-        finally:
-            session.close()
-
-
-users = Users()
-sep = DataSeparatorFromInsert(OriginData.original_data())
+print(f'output 5: {insert(user_accounts).values().compile(engine)}')
 
